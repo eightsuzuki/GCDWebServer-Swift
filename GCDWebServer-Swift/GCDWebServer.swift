82151c824c1db4a25aa278bff2bb7d850ea53bf2
eightsuzuki <suzukieight8@gmail.com>
2023-08-28 15:36:33 +0900

    Add GCDWebServerMatchBlock

--- GCDWebServer-Swift/GCDWebServer.swift
+++ GCDWebServer-Swift/GCDWebServer.swift
@@ -1,7 +1,7 @@
 /*
  Copyright (c) 2012-2019, Pierre-Olivier Latour
  All rights reserved.
-
+ 
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
  * Redistributions of source code must retain the above copyright
@@ -12,7 +12,7 @@
  * The name of Pierre-Olivier Latour may not be used to endorse
  or promote products derived from this software without specific
  prior written permission.
-
+ 
  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -24,30 +24,39 @@
  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+import Foundation
+
+public typealias GCDWebServerMatchBlock = (_ requestMethod: String, _ requestURL: URL, _ requestHeaders: [String: String], _ urlPath: String, _ urlQuery: [String: String]) -> String?
 
 class GCDWebServerHandler {
+
+  private var matchBlock: GCDWebServerMatchBlock?
+
+  init(mathcBlock: @escaping GCDWebServerMatchBlock) {
+    self.matchBlock = mathcBlock
+  }
 }
 
 public class GCDWebServer {
-
+    
   fileprivate var handlers: [GCDWebServerHandler]
-
+  
   public init() {
     handlers = []
   }
 
-  public func addHandler() {
-      let handler = GCDWebServerHandler()
-      handlers.insert(handler, at: 0)
-    }
-
-    public func removeAllHandlers() {
-      handlers.removeAll()
-    }
+  public func addHandler(with matckBlock: @escaping GCDWebServerMatchBlock) {
+    let handler = GCDWebServerHandler(mathcBlock: matckBlock)
+    handlers.insert(handler, at: 0)
   }
+
+  public func removeAllHandlers() {
+    handlers.removeAll()
+  }
+}
 /// Extenstion for tests
 extension GCDWebServer {
-
+  
   func handlersCount() -> Int {
     return handlers.count
   }
--- Tests/GCDWebServerTests.swift
+++ Tests/GCDWebServerTests.swift
@@ -1,17 +1,18 @@
 @testable import GCDWebServer_Swift
-
 import XCTest
-
 final class Tests: XCTestCase {
-
+  
   func testAddHandler() {
-      let server = GCDWebServer()
-      XCTAssertNotNil(server)
+    let server = GCDWebServer()
+    XCTAssertNotNil(server)
 
-      server.addHandler()
-      XCTAssertEqual(server.handlersCount(), 1)
-
-      server.removeAllHandlers()
-      XCTAssertEqual(server.handlersCount(), 0)
+    let matchBlock: GCDWebServerMatchBlock = { requestMethod, requestURL, requestHeaders, urlPath, urlQuery in
+      return ""
     }
+    server.addHandler(with: matchBlock)
+    XCTAssertEqual(server.handlersCount(), 1)
+
+    server.removeAllHandlers()
+    XCTAssertEqual(server.handlersCount(), 0)
   }
+}
